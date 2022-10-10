21104843ba714d07a98720fa2415132d870b40e2
Sergey <ranik07@bk.ru>
2022-10-10 21:31:00 +0300

    Задача 5

---
+++ Знакомство с Python/ДЗ1/5.py
@@ -0,0 +1,15 @@
+# Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+import math
+import os
+
+print("Введите координат х точки A: ")
+xA = int(input())
+print("Введите координат y точки A: ")
+yA = int(input())
+print("Введите координат х точки B: ")
+xB = int(input())
+print("Введите координат y точки B: ")
+yB = int(input())
+distance = round((math.sqrt(math.pow((xB-xA),2) + math.pow((yB-yA),2))),2)
+os.system('CLS')
+print(f'A ({xA},{yA}); B ({xB},{yB}) -> {distance}')
