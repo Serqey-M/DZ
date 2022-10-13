9245071e8c16eab48575b347fb96f9083d50d139
Sergey <ranik07@bk.ru>
2022-10-14 00:39:48 +0300

    задача 1

---
+++ Знакомство с Python/ДЗ2/1.py
@@ -0,0 +1,15 @@
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+import os
+
+number = float(input('Введите число: '))
+list = []
+for i in str(number):
+    list.append(i)
+list.remove('.')
+if list[0]=='-':
+    list.remove('-')
+sum = 0
+for i in list:
+    sum+=int(i)
+os.system('CLS')
+print(f'{number} -> {sum}')
