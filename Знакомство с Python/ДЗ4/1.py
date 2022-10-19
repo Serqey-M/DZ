bb98d5ceaa4c89d83b3a4a5b6a3bc846d2c1b2dc
Sergey <ranik07@bk.ru>
2022-10-20 01:18:30 +0300

    задача 1

--- Знакомство с Python/ДЗ4/1.py
+++ Знакомство с Python/ДЗ4/1.py
@@ -1,14 +1,14 @@
 # Вычислить число c заданной точностью d (число Пи)
 d = float(input('Задайте точность точность. d = '))
-n=3
+n = 3
 Pi1 = 4
 Pi = Pi1-4/n
-while abs(Pi-Pi1)>d:
-    n+=2
-    Pi1=Pi
-    Pi=Pi1+4/n
-    if abs(Pi-Pi1)>d:
-        n+=2
-        Pi1=Pi
-        Pi=Pi1-4/n
+while abs(Pi-Pi1) > d:
+    n += 2
+    Pi1 = Pi
+    Pi = Pi1+4/n
+    if abs(Pi-Pi1) > d:
+        n += 2
+        Pi1 = Pi
+        Pi = Pi1-4/n
 print(Pi)
