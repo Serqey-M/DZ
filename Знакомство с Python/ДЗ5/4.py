768464656a1071586a99814653f07659ae2ff562
Sergey <ranik07@bk.ru>
2022-10-31 00:20:50 +0300

    задача 4

--- Знакомство с Python/ДЗ5/4.py
+++ Знакомство с Python/ДЗ5/4.py
@@ -7,18 +7,25 @@ text1=''
 i=1
 while i<len(text)-1:
     if text[i]==text[i-1]:
-        j=1
-        while text[i]==text[i-1] and i<len(text)-1:
-            j+=1
-            i+=1
+        j=2
+        while text[i]==text[i-1] and i<len(text):
             print(f'i={i} j={j}')
-            temp=text1+str(j)+text[i-1]
+            temp=str(j)+text[i]
             print(temp)
+            i+=1
+            j+=1
+    elif text[i]==text[i+1]:
+        i+=1
+        temp=''
     else:
-        temp=text1+'1'+text[i]
+        temp='1'+text[i]
+        print(f'i={i} j={j}')
+        print(f'text1={text1} text{i}={text[i]}')
+        print(f'temp={temp}')
         i+=1
     text1+=temp
-    print(text1)
+    print(f'text1={text1}')
+    
 
 
 
