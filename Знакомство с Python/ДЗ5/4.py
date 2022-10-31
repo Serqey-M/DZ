8bb8d74fe095e804c3b9b96de49154b09c1eba40
Sergey <ranik07@bk.ru>
2022-10-31 23:51:22 +0300

    задача 4

--- Знакомство с Python/ДЗ5/4.py
+++ Знакомство с Python/ДЗ5/4.py
@@ -1,40 +1,53 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных. Входные и выходные данные хранятся в отдельных текстовых файлах.
-file = open('1.txt', 'r')
+def compression(text):
+    text1 = ''
+    i = 1
+    while i < len(text):
+        if text[i] == text[i-1]:
+            j = 2
+            while i < len(text) and text[i] == text[i-1]:
+                temp = str(j)+text[i]
+                i += 1
+                j += 1
+        elif (i == len(text)-1):
+            temp = '1'+text[i]
+            i += 1
+        elif text[i] == text[i+1]:
+            i += 1
+            temp = ''
+        else:
+            temp = '1'+text[i]
+            i += 1
+        text1 += temp
+    return text1
+
+
+def recovery(text):
+    i = 0
+    text2 = ''
+    while i < len(text)-1:
+        temp = ''
+        while (text[i].isdigit() == True):
+            temp += text[i]
+            i += 1
+        else:
+            temp = int(temp)
+        text2 += text[i]*temp
+        i += 1
+    return text2
+
+
+# file = open('1.txt', 'r')
+file = open('2.txt', 'r')
 text = file.read()
 file.close()
-print(len(text))
-text1=''
-i=1
-while i<len(text)-1:
-    if text[i]==text[i-1]:
-        j=2
-        while text[i]==text[i-1] and i<len(text):
-            print(f'i={i} j={j}')
-            temp=str(j)+text[i]
-            print(temp)
-            i+=1
-            j+=1
-    elif text[i]==text[i+1]:
-        i+=1
-        temp=''
-    else:
-        temp='1'+text[i]
-        print(f'i={i} j={j}')
-        print(f'text1={text1} text{i}={text[i]}')
-        print(f'temp={temp}')
-        i+=1
-    text1+=temp
-    print(f'text1={text1}')
-    
+print(text)
 
+# print(compression(text))
+print(recovery(text))
 
-
-    # print(f'i={i}')
-    # j=1
-    # while text[i]==text[i+1]:
-    #     print(f'j={j}')
-    #     j+=1
-    #     i+=1
-    #     print(f'{j} {text[i]}')
-    # i+=1
-# print(text1)
+# file2 = open('2.txt', 'w')
+file2 = open('1.txt', 'w')
+# file2.writelines(compression(text))
+file2.writelines(recovery(text))
+file.close()
